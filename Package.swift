989189af88bcd0a82362a2f8e3ab910827c2fe34
bowenwp <54548588+bowenwp@users.noreply.github.com>
2021-05-12 16:17:50 -0700

    Update Package.swift for 1.1.7

--- Package.swift
+++ Package.swift
@@ -1,1 +1,20 @@
+// swift-tools-version:5.3
+import PackageDescription
 
+let package = Package(
+  name: "PersonaInquirySDK",
+  platforms: [.iOS(.v11)],
+  products: [
+    .library(
+      name: "PersonaInquirySDK", 
+      targets: ["Persona"]
+    )
+  ],
+  targets: [
+    .binaryTarget(
+      name: "Persona",
+      url: "https://github.com/bowenwp/inquiry-ios/releases/download/1.1.7/Persona.xcframework.zip",
+      checksum: "9d691a9ffe122a60eb3e606be707e6b239d2c5af98d32d2ef2dfa273aac78e54"
+    )
+  ]
+)
